3396804b45798bcbc41c8d555a7e29ae765505bb
shoarora <shoarora@stanford.edu>
2016-10-27 12:28:04 -0700

    dumbass baseline

--- tldr.py
+++ tldr.py
@@ -7,6 +7,21 @@ from tldrlib import *
 from util import *
 from process_data import *
 
+
+def dumbassPredictor(data):
+	error = 0
+	for d in data:
+		firstSentence = d[0][0].split('.')[0]
+		prediction = None
+		if d[0][1] in firstSentence:
+			prediction = 1
+		else:
+			prediction = -1
+		if prediction != d[1]:
+			error +=1
+	print error, len(data), error/float(len(data))
+
+
 print "learning"
 
 data = get_data()
@@ -18,6 +33,8 @@ print 'currently using test data of all keywords'
 # testData = random.sample(data, len(data)/10)
 # trainingData = [d for d in data if d not in testData]
 print 'datasets separated'
-w = learnPredictor(trainingData, testData, keywordFeatureExtractor, 10, 0.01)
+dumbassPredictor(data)
+# w = learnPredictor(trainingData, testData, keywordFeatureExtractor, 10, 0.01)
 
-print w
+# print w
+
