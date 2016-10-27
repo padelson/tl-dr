a5bdd8b0d160936ff4508ebdb4d2f23a09e72538
Jeff Hara <jhara_mac@DN51qo3b.SUNet>
2016-10-27 03:49:35 -0700

    done

---
+++ tldr.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import tldrlib
+import util
+import process_data
+from tldrlib import *
+from util import *
+from process_data import *
+
+print "learning"
+
+def loadTraining
+
+def processData(numSamples=-1):
+    f1 = open('raw_data.txt', 'r')
+    f2 = open('processed_data.txt', 'w')
+    data = f1.read().split('\n')
+    f1.close()
+
+    rawCount = 0
+    entryCount = 0
+    for line in data:
