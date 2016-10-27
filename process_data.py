2503473a8dad1afd8e5a3fe5f9fa4a8f647aa57f
shoarora <shoarora@stanford.edu>
2016-10-27 04:16:14 -0700

    read line by line instead of all at once

--- process_data.py
+++ process_data.py
@@ -15,12 +15,11 @@ import json
 def process_data(num_samples=-1):
     f1 = open('raw_data.txt', 'r')
     f2 = open('processed_data.txt', 'w')
-    data = f1.read().split('\n')
-    f1.close()
-
+    
     raw_count = 0
     entry_count = 0
-    for line in data:
+    while (True):
+        line = f1.readline()
         if line == '' or (raw_count == num_samples and num_samples is not -1):
             break
 
@@ -50,6 +49,7 @@ def process_data(num_samples=-1):
             if entry_count % 100 == 0:
                 print 'entry ' + str(entry_count) + ' added'
 
+    f1.close()
     f2.close()
 
 
@@ -60,10 +60,10 @@ def process_data(num_samples=-1):
 # of example points, where each point is of the form: (article, word) , isKeyWord
 def get_data(num_samples=-1):
     f = open('processed_data.txt', 'r')
-    data = f.read().split('\n')
     entries = []
     count = 0
-    for line in data:
+    while (True):
+        line = f.readline()
         if line == '' or (count == num_samples and num_samples is not -1):
             break
 
@@ -86,10 +86,10 @@ def get_data(num_samples=-1):
 # of example points, where each point is of the form: (article, title, word) , isKeyWord
 def get_oracle_data(num_samples=-1):
     f = open('processed_data.txt', 'r')
-    data = f.read().split('\n')
     entries = []
     count = 0
-    for line in data:
+    while (True):
+        line = f.readline()
         if line == '' or (count == num_samples and num_samples is not -1):
             break
 
@@ -106,6 +106,6 @@ def get_oracle_data(num_samples=-1):
     return entries
 
 
-# process_data()
-# print get_data(1)
-# print get_oracle_data(1)
+# process_data(100)
+print get_data(100)
+# print get_oracle_data(100)
