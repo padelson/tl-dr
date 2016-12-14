04f7209b528aa6c3f52605083a47e4cdf8be1a14
Jeff Hara <jhara_mac@DN51qo69.SUNet>
2016-12-13 22:06:36 -0800

    testing keywords
    ads

--- sum_gen.py
+++ sum_gen.py
@@ -1,9 +1,28 @@
 from key_ex import *
+import util
 
+class SummaryMDP(util.MDP):
+    def __init__(self, keywords):
+        self.keywords = keywords
 
+    # Return the start state
+    # (empty string, remaining keywords)
+    def startState(self):
+        return ("", self.keywords)
 
+    # Return the set of actions possible from state
+    def actions(self, state):
+        pass
+
+# extract_keys returns map of pos to words
 def generate_summary(text, wordCounts, wikiCounts):
     keywords = extract_keys(text, wordCounts, wikiCounts)
+    nouns = sorted(keywords['NOUN'])
+    verbs = sorted(keywords['VERB'])
+    adjs = sorted(keywords['ADJ'])
+    print nouns[-10:]
+    print verbs[-10:]
+    print adjs[-10:]
     result = ''
 
     return result
